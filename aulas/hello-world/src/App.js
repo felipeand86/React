9175cd66be05e7e19edb81820c176414a1ded01e
Felipe André <felipe.tecpro26@gmail.com>
2025-06-05 23:38:56 -0300

    create react

---
+++ aulas/hello-world/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <main>
+        <section>
+          <p>
+            abc
+            default
+          </p>
+        </section>
+      </main>
+      gfr
+    </div>
+  );
+}
+
+export default App;
