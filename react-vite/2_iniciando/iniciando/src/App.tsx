bb36c0efb487bae33f1529024c91d5c474b0b6a3
Felipe André <felipe.tecpro26@gmail.com>
2025-06-26 21:42:36 -0300

    primeiro component

---
+++ react-vite/2_iniciando/iniciando/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FirstComponent from './components/FirstComponent.tsx'
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Conhecendo o React</h1>
+      <FirstComponent />
+    </div>
+  )
+}
+
+export default App
