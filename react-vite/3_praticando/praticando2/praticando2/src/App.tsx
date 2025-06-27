733525a4d43eace5ffe5c5b7214f80ed60cb5f6e
Felipe André <felipe.tecpro26@gmail.com>
2025-06-26 23:28:26 -0300

    criando ex contador

---
+++ react-vite/3_praticando/praticando2/praticando2/src/App.tsx
@@ -0,0 +1,16 @@
+import { Contador } from './components/Contador'
+import { useState } from 'react';
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Crie um botão que conte as clicadas feitas pelo usuário</h1>
+      <Contador/>
+    </>
+  )
+}
+
+export default App
